1b03018cc3e34914541b3a1e262cc0a244c5358c
tanushree-sharma <47197349+tanushree-sharma@users.noreply.github.com>
2019-11-21 17:14:58 -0500

    updated-script

--- get-tweets.py
+++ get-tweets.py
@@ -1,32 +1,41 @@
 # script to scrape tweets using a search keyword
 
 import GetOldTweets3 as got
+import csv
+import pandas as pd
 from textblob import TextBlob
 
-tweetCriteria = got.manager.TweetCriteria().setQuerySearch("military")\
-                                           .setMaxTweets(100)\
+max_tweets = 200
+
+tweetCriteria = got.manager.TweetCriteria().setQuerySearch("environment")\
+                                           .setMaxTweets(max_tweets)\
                                            .setNear("39.50, 98.35")\
                                            .setWithin("900mi")\
                                            .setSince("2017-01-20")\
-                                           .setUntil("2019-11-20")
+                                           .setUntil("2019-11-21")\
+                                           .setLang("en")
 
 num_tweets = 0
 polarity = 0
 
-for i in range(1,10):
 
+df = pd.DataFrame(columns=['tweet','military','healthcare','immigration','environment','gun control', 'economy','taxes',
+                           'education','foreign policy','polarity'])
+
+for i in range(1,max_tweets):
     tweet = got.manager.TweetManager.getTweets(tweetCriteria)[i]
 
     analysis = TextBlob(tweet.text)  # analyzes the string
 
-    if (analysis.polarity) != 0:
-        polarity = polarity + analysis.polarity
+    if (analysis.polarity) != 0 and ("http" not in tweet.text) and (".com" not in tweet.text):
+
+        df = df.append({'tweet': tweet.text , 'polarity' : analysis.polarity}, ignore_index=True)
+
         num_tweets = num_tweets + 1
-        print(tweet.text)  # .text turns it into a strings
-        print(analysis.polarity)
-        print("\n")
+        print(tweet.text)
 
 
-print("average polarity is")
-print (polarity/num_tweets)
 print(num_tweets)
+
+
+df.to_csv("/Users/tanushrees/Documents/environment_tweets.csv", index=False)
